2266b86fdd3d74044ad942d83d3cc368aabcbb81
Digi24-dev <digi7442@gmail.com>
2024-07-03 14:56:52 +0900

    <UPDATE> - OpenVidu 컴포넌트 추가 - axios 컴포넌트화 예정

---
+++ src/components/OpenVidu/AudioComponent.tsx
@@ -0,0 +1,25 @@
+import { LocalAudioTrack, RemoteAudioTrack } from "livekit-client";
+import { useEffect, useRef } from "react";
+import React from "react";
+
+interface AudioComponentProps {
+  track: LocalAudioTrack | RemoteAudioTrack;
+}
+
+function AudioComponent({ track }: AudioComponentProps) {
+  const audioElement = useRef<HTMLAudioElement | null>(null);
+
+  useEffect(() => {
+    if (audioElement.current) {
+      track.attach(audioElement.current);
+    }
+
+    return () => {
+      track.detach();
+    };
+  }, [track]);
+
+  return <audio ref={audioElement} id={track.sid} />;
+}
+
+export default AudioComponent;
